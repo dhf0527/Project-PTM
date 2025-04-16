71ba0263ab98cbc8c217efc900716aea1c0f3b0a
dhf0527 <rbals0527@naver.com>
2025-04-16 12:58:39 +0900

    버그 수정
    씬이 바뀌어도 속도가 초기화 안 되는 버그 수정

--- Assets/1. Script_New/UI/Main/SceneChangeManager.cs
+++ Assets/1. Script_New/UI/Main/SceneChangeManager.cs
@@ -12,10 +12,12 @@ public class SceneChangeManager : MonoBehaviour
     public void OnToMainScene()
     {
         SceneManager.LoadScene(mainSceneName);
+        Time.timeScale = 1f;
     }
 
     public void OnToDungeonScene()
     {
         SceneManager.LoadScene(dungeonSceneName);
+        Time.timeScale = 1f;
     }
 }
