ea62f38e738097841e96abff6b83c305bf4b347b
dhf0527 <rbals0527@naver.com>
2025-07-05 15:34:18 +0900

    터치음 피드백
    터치음 누락부분 추가
    테스트용 효과음 버튼 추가
    apk빌드 최신화

--- Assets/1. Script_New/UI/Main/Meal/Meal_Card.cs
+++ Assets/1. Script_New/UI/Main/Meal/Meal_Card.cs
@@ -38,8 +38,9 @@ public class Meal_Card : MonoBehaviour
         mask_Go.SetActive(isActive);
     }
 
-    public void OnClickSound()
+    public void OnClickSound(bool isOn)
     {
-        AudioManager.instance.PlayerSfx(SFX_Enum.Touch);
+        if(isOn)
+            AudioManager.instance.PlayerSfx(SFX_Enum.Touch);
     }
 }
