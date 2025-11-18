3f5cd25c8175bd56bdbc63bd6763e2eb9a0a46b8
dhf0527 <rbals0527@naver.com>
2025-11-18 21:56:39 +0900

    하드모드 스프라이트 변경 구현
    스와이프/버튼 클릭시 변경

--- Assets/1. Script_New/UI/ImageAnimation.cs
+++ Assets/1. Script_New/UI/ImageAnimation.cs
@@ -6,13 +6,18 @@ using UnityEngine.UI;
 public class ImageAnimation : MonoBehaviour
 {
     [SerializeField] List<Sprite> sprites;
+    [SerializeField] List<Sprite> night_Sprites;
     [SerializeField] float period;
     [SerializeField] float period_dissolve;
     Image img;
-    [SerializeField]Image pre_img;
+    [SerializeField] Image pre_img;
+    [SerializeField] GameObject next_Obj;
+    [SerializeField] GameObject back_Obj;
+
     float curTime;
     int index = 0;
 
+    bool isNight;
     private void Awake()
     {
         img = GetComponent<Image>();
@@ -29,9 +34,7 @@ public class ImageAnimation : MonoBehaviour
             if (index >= sprites.Count)
                 index -= sprites.Count;
 
-            img.sprite = sprites[(index + 1) % sprites.Count];
-            if(pre_img)
-                pre_img.sprite = sprites[index];
+            SetSprite();
             curTime = 0;
         }
 
@@ -41,5 +44,28 @@ public class ImageAnimation : MonoBehaviour
             tmp_Color.a = 1 - curTime / period_dissolve;
             pre_img.color = tmp_Color;
         }
+    }
+
+    public void SetSprite()
+    {
+        if (isNight)
+        {
+            img.sprite = night_Sprites[(index + 1) % night_Sprites.Count];
+            if (pre_img)
+                pre_img.sprite = night_Sprites[index];
+        }
+        else
+        {
+            img.sprite = sprites[(index + 1) % sprites.Count];
+            if (pre_img)
+                pre_img.sprite = sprites[index];
+        }
+    }
+
+    public void SetIsNight(bool setBool)
+    {
+        isNight = setBool;
+        next_Obj.SetActive(!setBool);
+        back_Obj.SetActive(setBool);
     }
 }
---
+++ Assets/1. Script_New/UI/Main/SwipeWolrdMap.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwipeWolrdMap : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private Vector2 startPos;
+    private float startTime;
+    private Vector2 endPos;
+    private float endTime;
+
+    public float minSwipeSpeed = 800f; // 픽셀/초 기준 속도
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        startPos = eventData.position;
+        startTime = Time.unscaledTime; //
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        endPos = eventData.position;
+        endTime = Time.unscaledTime;
+
+        DetectSwipe();
+    }
+
+    void DetectSwipe()
+    {
+        Vector2 swipe = endPos - startPos;
+        float duration = endTime - startTime;
+
+        if (duration <= 0f) return;
+
+        float speed = swipe.magnitude / duration; // 속도 계산
+
+        // 속도가 기준보다 느리면 스와이프 취급 안 함
+        if (speed < minSwipeSpeed)
+            return;
+
+        // 방향 판정
+        if (Mathf.Abs(swipe.x) > Mathf.Abs(swipe.y))
+        {
+            if (swipe.x > 0)
+                OnSwipeRight();
+            else
+                OnSwipeLeft();
+        }
+    }
+
+    void OnSwipeLeft() 
+    {
+        GetComponent<ImageAnimation>().SetIsNight(true);
+    }
+    void OnSwipeRight() 
+    {
+        GetComponent<ImageAnimation>().SetIsNight(false);
+    }
+}
