066085587ede688decbbebb2de8aa83b60b66f62
dhf0527 <rbals0527@naver.com>
2026-02-19 20:03:52 +0900

    관통/무속성 변수 관련 버그 수정

--- Assets/1. Script_New/Manager/DunGeonManager_New.cs
+++ Assets/1. Script_New/Manager/DunGeonManager_New.cs
@@ -310,6 +310,47 @@ public class DunGeonManager_New : MonoBehaviour
         MealData md;
         if (GameManager.Instance.CheckAppliedMeal(8, out md))
             unit.unitStatData_st.attack_Plus += (EnemySpawnManager.instance.cur_Wave + 1) * (md.mealValue);
+        //유리비늘 생선구이
+        if (GameManager.Instance.CheckAppliedMeal(0, out md) && unit.ud.armor == 0)
+            unit.unitStatData_st.armor_Plus += (int)md.mealValue;
+        //칠면조 바비큐
+        if (GameManager.Instance.CheckAppliedMeal(3, out md))
+            unit.unitStatData_st.max_Hp_Plus += md.mealValue;
+        //파인애플 피자
+        if (GameManager.Instance.CheckAppliedMeal(9, out md))
+        {
+            unit.unitStatData_st.isFixed_AttackSpeed = true;
+            unit.unitStatData_st.fixedAttackSpeed = md.mealValue;
+        }
+        //로즈베리 케이크
+        if (GameManager.Instance.CheckAppliedMeal(104, out md))
+        {
+            unit.isNoTypeDamage = true;
+            unit.isPenetration = true;
+        }
+        //불사조 닭발
+        if (GameManager.Instance.CheckAppliedMeal(100, out md))
+            unit.unitStatData_st.attack_PlusPercent += md.mealValue2;
+        //든든 국밥
+        if (GameManager.Instance.CheckAppliedMeal(101, out md))
+        {
+            unit.unitStatData_st.avoidance_Plus += (int)md.mealValue;
+            unit.unitStatData_st.accuracy_Plus += (int)md.mealValue2;
+        }
+        //정체불명 햄버거
+        if (GameManager.Instance.CheckAppliedMeal(102, out md))
+            unit.unitStatData_st.cost_MinusPercent += md.mealValue;
+        //드워프 맥주
+        if (GameManager.Instance.CheckAppliedMeal(105, out md))
+        {
+            unit.unitStatData_st.damageReduction_PlusPercent -= md.mealValue;
+            unit.unitStatData_st.attackBoost_PlusPercent += md.mealValue2;
+        }
+        //드래곤알 오믈렛
+        if (GameManager.Instance.CheckAppliedMeal(200, out md))
+        {
+            unit.unitStatData_st.cost_MinusPercent += md.mealValue;
+        }
 
         onStageUnits_Test.Add(unit);
 
